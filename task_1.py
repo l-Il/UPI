c72fc3c17fdaa4736ced8a76b5383f4dd26087a6
German Chursinov <chursinov.g.s@edu.mirea.ru>
2024-05-10 17:08:06 +0300

    Заменил .py на .ipynb

--- task_1.py
+++
@@ -1,6 +0,0 @@
-def task_1(number: int) -> dict:
-  return {'oct': oct(number), 'hex': hex(number)}
-
-
-for _ in range(30):
-  print(f"Представление числа {_} в восьмеричной системе счисления: {task_1(_)['oct']}, представление числа {_} в шестнадцатеричной системе счисления: {task_1(_)['hex']}.")
